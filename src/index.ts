2c225b8ebdab483201d3ac2d1473c1b2bfd7e613
Gilson Gabriel <gilson@codemastersolucoes.com>
2025-07-13 14:08:44 -0300

    feat: add initial typescript file and build script

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Greets the world.
+ *
+ * @returns A greeting message.
+ */
+export function helloWorld(): string {
+  const message = 'Hello, World!'
+  console.log(message)
+  return message
+}
+
+helloWorld()
